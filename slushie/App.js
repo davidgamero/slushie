bb9eba6bdd4fb3fb3ac00aaba14441acce989505
David Gamero <david340804@gmail.com>
2019-06-05 18:51:47 -0400

    tab icons

--- slushie/App.js
+++ slushie/App.js
@@ -5,11 +5,34 @@ import {
 } from 'react-native';
 import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
 import HomeScreen from './screens/home';
-import BudgetScreen from './screens/budget'
+import BudgetScreen from './screens/budget';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const TabNavigator = createBottomTabNavigator({
   Home: HomeScreen,
   Budget: BudgetScreen,
-});
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        const { routeName } = navigation.state;
+        let IconComponent = Ionicons;
+        let iconName;
+        if (routeName === 'Home') {
+          iconName = `ios-information-circle${focused ? '' : '-outline'}`;
+        } else if (routeName === 'Budget') {
+          iconName = `ios-options`;
+        }
+
+        // You can return any component that you like here!
+        return <IconComponent name={iconName} size={25} color={tintColor} />;
+      },
+    }),
+    tabBarOptions: {
+      activeTintColor: 'tomato',
+      inactiveTintColor: 'gray',
+    },
+  }
+);
 
 export default createAppContainer(TabNavigator);

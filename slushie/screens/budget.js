3f931fa78394be74b399887f9060edfdb87beec5
Annika Zdon <annika.zdon@capitalone.com>
2019-06-05 17:39:55 -0400

    made first two main screens

---
+++ slushie/screens/budget.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
+
+class BudgetScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Let's budget ladies!</Text>
+      </View>
+    );
+  }
+}

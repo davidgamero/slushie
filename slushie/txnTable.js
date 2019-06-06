1a7941d7759a3775a9fdfee504c29ccace57258f
David Gamero <david340804@gmail.com>
2019-06-06 02:50:07 -0400

    txn

--- slushie/txnTable.js
+++ slushie/txnTable.js
@@ -10,10 +10,16 @@ export default class TxnTable extends Component {
       tableHead: ['Date', 'Amount', 'Category'],
       tableData: []
     }
+    
+  }
+
+  componentDidMount() {
+    this.getData();
   }
 
   getData() {
     txns = data.tableData; 
+    finalData = []
     for(i = 0; i < 20; i++) {
         let row = []
         let date = txns[i].date;
@@ -22,11 +28,20 @@ export default class TxnTable extends Component {
         row.push(date);
         row.push(amount);
         row.push(cat);
-        this.tableData.push(row);
+        finalData.push(row)
     }
+    this.setState({
+      tableData: finalData,
+    })
   }
 
   render() {
+    const tstyles = StyleSheet.create({
+      container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff' },
+      head: { height: 40, backgroundColor: '#f1f8ff' },
+      text: { margin: 6 }
+    });
+
     const state = this.state;
     return (
       <View style={tstyles.container}>

e7fd35dbaee02d8c83866c0e897b394b92588957
David Gamero <david340804@gmail.com>
2019-06-05 20:54:13 -0400

    fuckin chartsss

--- slushie/screens/home.js
+++ slushie/screens/home.js
@@ -4,11 +4,26 @@ import {
   View,
   Button
 } from 'react-native';
+import AreaSpline from '../d3chart/charts/AreaSpline';
+import Pie from '../d3chart/charts/Pie';
+import Theme from '../d3chart/theme';
+import data from '../d3chart/data';
 
 class HomeScreen extends React.Component {
   render() {
+    const height = 200;
+    const width = 500;
+
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Pie
+            pieWidth={150}
+            pieHeight={150}
+            //onItemSelected={this._onPieItemSelected}
+            colors={Theme.colors}
+            width={width}
+            height={height}
+            data={data.spendingsLastMonth} />
         {/* other code from before here */}
         <Button
           title="Go to Budget"
